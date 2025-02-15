3185ab29299c685c84fcb2ee2adccd2d24b6f66c
Bugrahan Arslan <bugrahan.arslan3@huawei-partners.com>
2025-02-15 17:36:56 +0300

    Update JenkinsFile.groovy

--- jenkins/JenkinsFile.groovy
+++ jenkins/JenkinsFile.groovy
@@ -9,6 +9,7 @@ pipeline {
         }
         
         stage('terraform init') {
+        steps {
                 sh 'cd terraform; terraform init -no-color'
             }
         }
@@ -19,8 +20,6 @@ pipeline {
                     usernamePassword(credentialsId: 'huaweicloudcreds', passwordVariable: 'HW_SECRET_KEY', usernameVariable: 'HW_ACCESS_KEY')
                 ]) {
                 sh '''
-                    echo "HW_ACCESS_KEY: $HW_ACCESS_KEY"
-                    echo "HW_SECRET_KEY: $HW_SECRET_KEY"
                     cd terraform; terraform apply -no-color -var huaweicloud_access_key=$HW_ACCESS_KEY -var huaweicloud_secret_key=$HW_SECRET_KEY --auto-approve
                 '''
                 }
